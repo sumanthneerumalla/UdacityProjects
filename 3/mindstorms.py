b0a46cfd8c4151112cf255d26338de69c2f0e730
Sumanth Neerumalla <sumanth42gold@gmail.com>
2015-08-12 12:32:32 -0400

    added a while loop to continuously draw

--- 3/mindstorms.py
+++ 3/mindstorms.py
@@ -1,3 +1,24 @@
 import turtle
 
-def drawSquare:
+def drawSquare():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.shape("classic")
+    brad.color("black")
+    brad.speed(2)
+    while True:
+       
+        brad.forward(100)
+        brad.right(90)
+        brad.forward(100)
+        brad.right(90)
+        brad.forward(100)
+        brad.right(90)
+        brad.forward(100)
+        brad.right(90)
+
+    window.exitonclick()
+
+drawSquare()
