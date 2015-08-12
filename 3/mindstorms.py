ff3d49dacc822a1b7cf330fca1e475c26398e32d
Sumanth Neerumalla <sumanth42gold@gmail.com>
2015-08-12 13:21:43 -0400

    moved previous code to testing.py because it wasn't really needed

--- 3/mindstorms.py
+++ 3/mindstorms.py
@@ -1,22 +1,36 @@
 import turtle
 import random
 
-def drawSquare():
+def drawScreen():
     window = turtle.Screen()
     window.bgcolor("red")
+    window.exitonclick()
 
+def drawSquare():
     brad = turtle.Turtle()
     brad.shape("classic")
     brad.color("black")
     brad.speed(2)
 
+    for i in range(0,4):
+        brad.forward(100)
+        brad.right(90)
 
 
-    while True :
-        distance = random.randint(20,100)
-        direction = random.randint(0,100)
-        brad.forward(distance)
-        brad.right(direction)
+
+def drawCircle():
+    angie = turtle.Turtle
+    angie.shape("turtle")
+    angie.color("white")
+    angie.setx(100)
+    angie.setx(100)
+    angie.circle(100)
+
+drawScreen()
+drawSquare()
+drawCircle()
+
+
 
 
 
---
+++ 3/testing.py
@@ -0,0 +1,19 @@
+import turtle
+import random
+
+def drawSquare():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.shape("classic")
+    brad.color("black")
+    brad.speed(2)
+
+
+
+    while True :
+        distance = random.randint(20,100)
+        direction = random.randint(0,100)
+        brad.forward(distance)
+        brad.right(direction)
