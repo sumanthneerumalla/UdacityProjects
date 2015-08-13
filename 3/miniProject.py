ea30670115204c33d990a385e84b34d2e7ba4810
Sumanth Neerumalla <sumanth42gold@gmail.com>
2015-08-12 22:45:40 -0400

    updated gitnore

--- 3/miniProject.py
+++ 3/miniProject.py
@@ -1,1 +1,22 @@
-__author__ = 'suman_000'
+import turtle
+
+
+brad = turtle.Turtle()
+window = turtle.Screen()
+
+brad.speed(0)
+for i in range(36):
+    brad.left(35)
+    brad.forward(50)
+    brad.right(35)
+    brad.forward(50)
+    brad.right(145)
+    brad.forward(50)
+    brad.right(35)
+    brad.forward(50)
+    brad.right(10)
+
+brad.setheading(270)
+brad.forward(300)
+
+window.exitonclick()
