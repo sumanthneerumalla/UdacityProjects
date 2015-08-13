fae0f0a975baf8f8e94d326b4d5c0d22fba5649b
Sumanth Neerumalla <sumanth42gold@gmail.com>
2015-08-13 00:02:45 -0400

    tested out sign wdyl's profanity checker and pirate speech converter with urllib

--- 5/checkProfanity.py
+++ 5/checkProfanity.py
@@ -1,5 +1,27 @@
+import urllib
+
 def readText():
     quotes = open("C:\\Sumanth\\pythonprojects\\Udacity\\5\moviequotes.txt", 'r')
     contents = quotes.read()
-    print(contents)
+    checkProfanity(contents)
+    text2Pirate(contents)
+
+
+def checkProfanity(contents):
+    connection = urllib.urlopen("http://www.wdyl.com/profanity?q="+contents)
+    output = connection.read()
+    if "true" in output:
+        print "Profanity alert!"
+    elif "false" in output:
+        print 'No curse words found!'
+    else:
+        print 'Something happened....'
+    connection.close()
+
+def text2Pirate(contents):
+    connection = urllib.urlopen("http://isithackday.com/arrpi.php?text="+contents)
+    output = connection.read()
+    print output
+    connection.close()
+
 readText()
